51c8983fa3b486dc55e2da424d41a28c039b4f4e
wenbo68 <wenboliu@live.unc.edu>
2023-09-17 14:59:34 -0400

    cloned from github

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
-import { formatDistanceToNow } from "date-fns";
+import React from 'react';
+import { render } from 'react-dom';
+import App from './components/App';
 
-const date = "2001-05-26 10:00:00";
-document.body.textContent = `${formatDistanceToNow(new Date(date))} ago i was born`;
+render(<App />, document.getElementById('app'));
+
+if (module.hot) {
+  module.hot.accept(function() {
+    window.location.reload();
+  });
+}
